6d356157c9a5a9d3742989fa2b65c3c6ca51b0a2
JensLegarthRyge <90308706+JensLegarthRyge@users.noreply.github.com>
2021-09-08 11:26:11 +0200

    Initialized CalculatorExercise

--- src/CalculatorAddition.java
+++ src/CalculatorAddition.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class CalculatorAddition {
-    public static double add (double valueOne, double valueTwo){
-        double valuesAdded=valueOne+valueTwo;
+    public static int add (int valueOne, int valueTwo){
+        int valuesAdded=valueOne+valueTwo;
         return valuesAdded;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("//////////// LOMMEREGNER ////////////");
-        double firstValue = scanner.nextDouble();
-        System.out.println(firstValue);
-        String method = scanner.nextLine();
-        System.out.println();
-        double secondValue = scanner.nextDouble();
+        System.out.println("//////////// LOMMEREGNER ////////////"+"\n"+
+                "Indtast dit første tal: ");
+        int firstValue = scanner.nextInt();
+        System.out.println("Indtast dit næste tal: ");
+        int secondValue = scanner.nextInt();
+        System.out.println("Resultatet er: "+add(firstValue,secondValue));
     }
 }
