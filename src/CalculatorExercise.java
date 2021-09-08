211670f2138307cd589c4c3b608c9c359131e468
Healsu <timmie2500@gmail.com>
2021-09-08 10:44:44 +0200

    Tilføjet sout igen

--- src/CalculatorExercise.java
+++ src/CalculatorExercise.java
@@ -2,5 +2,6 @@ public class CalculatorExercise {
     public static void main(String[] args) {
         System.out.println("Test123");
         System.out.println("Hallo");
+        System.out.println("Frikadeller");
     }
 }
