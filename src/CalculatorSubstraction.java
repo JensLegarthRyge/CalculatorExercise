d1b1bfb7288f0ee8389ea464ecdcdb2655240725
Healsu <timmie2500@gmail.com>
2021-09-08 11:12:30 +0200

    Ændring på koden

--- src/CalculatorSubstraction.java
+++ src/CalculatorSubstraction.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class CalculatorSubstraction {
     public static void main(String[] args) {
         //Minus lommeregner
-        System.out.println("Welcome to the substraction calculator");
+       /* System.out.println("Welcome to the substraction calculator");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write your first number here: ");
         int fNumb = scanner.nextInt();
@@ -19,6 +19,21 @@ public class CalculatorSubstraction {
         int result = first-second;
 
         System.out.println("Your result gonna be "+result);
+*/
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write your first number here: ");
+        int fNumb = scanner.nextInt();
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Write your second number here: ");
+        int lNumb = scanner1.nextInt();
+
+        int result = (fNumb,lNumb);
+    }
+    public static substraction(int first, int second){
+
 
     }
 }
