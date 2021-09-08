ace9bb64c6ee96cd1862d7e201384721b34a6243
Healsu <timmie2500@gmail.com>
2021-09-08 10:58:02 +0200

    Tilføjet kode

--- src/CalculatorSubstraction.java
+++ src/CalculatorSubstraction.java
@@ -2,5 +2,24 @@ import java.util.Scanner;
 public class CalculatorSubstraction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Første nummer ind her: ");
+        int fNumb = scanner.nextInt();
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Andet nummer ind her: ");
+        int lNumb = scanner1.nextInt();
+
+
+
+
+
+
+    }
+    public static void subtraction(int first, int second){
+
+        int result = first-second;
+
+        System.out.println("Your result gonna be "+result);
+
     }
 }
